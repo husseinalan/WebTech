9afd2a84ef52c10e80bf319535a4b5ec2730abc8
Besitzer <basselghorab2100@gmail.com>
2023-04-23 23:03:43 +0200

    erste Meilenstein

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # WebTech
+
+## Das Projekt ist Karteikarte als Webseite zu erstellen .
+
+## Die Webseite soll die Folgenden Funktionen haben 
+
+## Die Nutzer sollen ein Konto erstellen können und es soll auch möglich sein Ordner zu erstellen und 
+## mit dem Unterordner. 
+
+## Ein Suchfeld soll vorhanden sein um nach bestimmten Themen oder Ordner zu suchen 
+
---
+++ src/main/java/de/htwberlin/webtech/Webtechnologie/WebtechnologieApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.Webtechnologie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechnologieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechnologieApplication.class, args);
+	}
+
+}
